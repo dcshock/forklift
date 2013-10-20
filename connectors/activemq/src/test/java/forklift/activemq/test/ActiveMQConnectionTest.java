569c1dfb03f319c08c19b9a5d9b130e8c64b2fd9
Matt Conroy <mconroy@sofi.org>
2013-10-20 15:27:34 -0600

    - add spinup test.

--- connectors/activemq/src/test/java/forklift/activemq/test/ActiveMQConnectionTest.java
+++ connectors/activemq/src/test/java/forklift/activemq/test/ActiveMQConnectionTest.java
@@ -1,11 +1,14 @@
 package forklift.activemq.test;
 
-import org.junit.Assert;
+import org.apache.activemq.broker.BrokerService;
 import org.junit.Test;
 
 public class ActiveMQConnectionTest {
     @Test
-    public void spinup() {
-        Assert.fail();
+    public void spinup() throws Exception {
+        BrokerService activemq = new BrokerService();
+        activemq.addConnector("tcp://127.0.0.1:61617");
+        activemq.start();
+        activemq.stop();
     }
 }
