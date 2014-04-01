225b0eeaa6bfbb90acadf1d65ce366d70e147dc7
Matt Conroy <mconroy@sofi.org>
2014-03-31 20:40:05 -0600

    introduce mockito to help isolate testing of packages. - add test to ensure consumer deployment events are covered.

--- core/src/test/java/forklift/consumer/ConsumerDeploymentEventsTest.java
+++ core/src/test/java/forklift/consumer/ConsumerDeploymentEventsTest.java
@@ -4,26 +4,31 @@ import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.google.common.io.Files;
 
 import forklift.ForkliftTest;
+import forklift.deployment.Deployment;
 import forklift.deployment.DeploymentWatch;
 
 public class ConsumerDeploymentEventsTest {
     @Test
-    public void deploy() 
+    public void consumerDeployEvent()
       throws IOException {
+        final ConsumerDeploymentEvents events = Mockito.mock(ConsumerDeploymentEvents.class);
         final File deployDir = Files.createTempDir();
-        final DeploymentWatch watch = new DeploymentWatch(deployDir, new ConsumerDeploymentEvents());
-      
+        final DeploymentWatch watch = new DeploymentWatch(deployDir, events);
+
         File deployFile = File.createTempFile("test", ".jar", deployDir);
         deployFile.deleteOnExit();
-        
+
         Files.copy(ForkliftTest.testJar(), deployFile);
         watch.run();
-        
+        Mockito.verify(events).onDeploy(Mockito.any(Deployment.class));
+
         deployFile.delete();
         watch.run();
+        Mockito.verify(events).onUndeploy(Mockito.any(Deployment.class));
     }
 }
