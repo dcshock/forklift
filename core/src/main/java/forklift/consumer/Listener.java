b15cf6a07674d530b67ea0ad84d4cbeaeafd17bd
Matt Conroy <mconroy@sofi.org>
2014-06-08 12:03:07 -0600

    - Handle messages by sending them to the msg handler.

--- core/src/main/java/forklift/consumer/Listener.java
+++ core/src/main/java/forklift/consumer/Listener.java
@@ -1,5 +1,8 @@
 package forklift.consumer;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -10,6 +13,7 @@ import javax.jms.MessageConsumer;
 import forklift.connectors.ConnectorException;
 import forklift.connectors.ForkliftConnectorI;
 import forklift.connectors.ForkliftMessage;
+import forklift.decorators.OnMessage;
 import forklift.decorators.Queue;
 import forklift.decorators.Topic;
 
@@ -24,6 +28,8 @@ public class Listener implements Runnable {
     private MessageConsumer consumer;
     private String name;
 
+    private List<Method> methods = new ArrayList<Method>();
+
     public Listener(Queue queue, Topic topic, Class<?> msgHandler,
                     ForkliftConnectorI connector) {
         super();
@@ -35,6 +41,12 @@ public class Listener implements Runnable {
     }
 
     public void listen() {
+        // Look for all methods that need to be called when a
+        // message is received.
+        for (Method m : msgHandler.getDeclaredMethods())
+            if (m.isAnnotationPresent(OnMessage.class))
+                methods.add(m);
+
         try {
             if (topic != null)
                 consumer = connector.getTopic("topic://" + topic.value());
@@ -59,12 +71,18 @@ public class Listener implements Runnable {
         try {
             running.set(true);
 
-            Message m;
-            while (running.get() && (m = consumer.receive()) != null) {
-                ForkliftMessage msg = connector.jmsToForklift(m);
+            Message jmsMsg;
+            while (running.get() && (jmsMsg = consumer.receive()) != null) {
+                final ForkliftMessage msg = connector.jmsToForklift(jmsMsg);
                 try {
+                    final Object handler = msgHandler.newInstance();
+                    for (Method m : methods) {
+                        // Send the message to each handler.
+                        m.invoke(handler, new Object[] {});
+                    }
+
                     msg.getMsg();
-                    m.acknowledge();
+                    jmsMsg.acknowledge();
                 } catch (Exception e) {
                     // Avoid acking a msg that hasn't been processed successfully.
                 }
