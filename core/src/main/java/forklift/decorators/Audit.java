3e01a8d2067ceef4d2a43b430902343fd52dc6ab
dthompson <dthompson@sofi.org>
2014-12-06 10:49:06 -0700

    Add the Audit annotation for processors.

---
+++ core/src/main/java/forklift/decorators/Audit.java
@@ -0,0 +1,19 @@
+package forklift.decorators;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Instruct a forklift to automatically send audit messages for a processor to the audit queue.
+ */
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Audit {
+
+}
