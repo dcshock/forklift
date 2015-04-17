6f5e4f167ceaca71971a1860ebff13a1f4b5b21a
Matt Conroy <mconroy@sofi.org>
2015-04-16 21:25:03 -0600

    - Deploy jars of jars or zips of jars.

--- core/src/main/java/forklift/deployment/Deployment.java
+++ core/src/main/java/forklift/deployment/Deployment.java
@@ -1,17 +1,24 @@
 package forklift.deployment;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.reflections.Reflections;
-import org.reflections.util.ConfigurationBuilder;
-
 import forklift.classloader.ChildFirstClassLoader;
 import forklift.decorators.Queue;
 import forklift.decorators.Topic;
+import org.reflections.Reflections;
+import org.reflections.util.ConfigurationBuilder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 public class Deployment {
     private Set<Class<?>> queues = new HashSet<Class<?>>();
@@ -26,10 +33,25 @@ public class Deployment {
     }
 
     public Deployment(File deployedFile)
-      throws MalformedURLException {
+      throws IOException {
         this.deployedFile = deployedFile;
 
-        final URL[] urls = new URL[] {deployedFile.toURI().toURL()};
+        // Read jars out of the deployed file.
+        final JarFile jar = new JarFile(deployedFile);
+        final List<URL> jarUrls = jar.stream().filter(entry -> {
+           return entry.getName().endsWith(".jar") || entry.getName().endsWith(".zip");
+        }).map(entry -> {
+            try {
+                return jarEntryAsUri(jar, entry).toURL();
+            } catch (Exception e) {
+                return null;
+            }
+        }).collect(Collectors.toList());
+        jarUrls.add(deployedFile.toURI().toURL());
+
+        // TODO - we should cleanup temp jars when the deploy is thrown away.
+
+        final URL[] urls = jarUrls.toArray(new URL[0]);
 
         // Assign a new classloader to this deployment.
         cl = new ChildFirstClassLoader(urls, getClass().getClassLoader());
@@ -84,5 +106,33 @@ public class Deployment {
         return "Deployment [queues=" + queues + ", topics=" + topics + ", cl="
                 + cl + ", deployedFile=" + deployedFile + ", reflections="
                 + reflections + "]";
+    }
+
+    private static URI jarEntryAsUri(JarFile jarFile, JarEntry jarEntry)
+      throws IOException {
+        InputStream input = null;
+        OutputStream output = null;
+        try {
+            String name = jarEntry.getName().replace('/', '_');
+            int i = name.lastIndexOf(".");
+            String extension = i > -1 ? name.substring(i) : "";
+            File file = File.createTempFile(
+                name.substring(0, name.length() - extension.length()) +
+                ".", extension);
+            file.deleteOnExit();
+            input = jarFile.getInputStream(jarEntry);
+            output = new FileOutputStream(file);
+            int readCount;
+            byte[] buffer = new byte[4096];
+            while ((readCount = input.read(buffer)) != -1) {
+                output.write(buffer, 0, readCount);
+            }
+            return file.toURI();
+        } finally {
+            if (input != null)
+                input.close();
+            if (output != null)
+                output.close();
+        }
     }
 }
