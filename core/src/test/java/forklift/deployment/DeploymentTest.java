4d1d8557215f023552a01037e13d7ce162421573
dthompson <dthompson@sofi.org>
2015-04-27 09:52:13 -0600

    Add new tests and supportive files for the tests to check deployment.

---
+++ core/src/test/java/forklift/deployment/DeploymentTest.java
@@ -0,0 +1,88 @@
+package forklift.deployment;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.jar.Attributes;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+
+import org.junit.Test;
+
+public class DeploymentTest {
+    @Test(expected = IOException.class)
+    public void testNullDeployment() throws IOException {
+        new Deployment(null);
+    }
+
+    // Any kind of bad Jar file should only throw IOExcpetion otherwise we may bring down the system
+    @Test(expected = IOException.class)
+    public void testEmptyDeployment() throws IOException {
+        File f = File.createTempFile("test", ".txt");
+
+        try {
+            new Deployment(f);
+        } finally {
+            // Don't leave test files around
+            f.delete();
+        }
+    }
+
+    @Test
+    public void testManifestOnlyDeploy() throws IOException {
+        File f = File.createTempFile("test", ".jar");
+
+        Manifest manifest = new Manifest();
+        manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        JarOutputStream target = new JarOutputStream(new FileOutputStream(f.getAbsolutePath()), manifest);
+        target.close();
+
+        try {
+            Deployment d = new Deployment(f);
+            assertEquals(f.getName(), d.getDeployedFile().getName());
+            assertTrue(d.isJar());
+            assertEquals(0, d.getQueues().size());
+            assertEquals(0, d.getTopics().size());
+            // Make sure we don't accidentally delete on deploy
+            assertTrue(f.exists());
+        } finally {
+            f.delete();
+        }
+    }
+
+    // Now using a prebuilt test jar -- found in the forklift-test-consumer
+    // make sure that we can deploy a jar that contains multiple classes that have some consumers
+    // that consume queues and some consuming topics
+    @Test
+    public void testDeployJar() throws IOException {
+    	File f = new File("src/test/resources/forklift-multitq-consumer-0.1.jar");
+        Deployment d = new Deployment(f);
+        assertNotNull(d);
+        assertTrue(d.isJar());
+        assertEquals(2, d.getQueues().size());
+        assertEquals(2, d.getTopics().size());
+        // Make sure we don't accidentally delete on deploy
+        assertTrue(f.exists());
+    }
+
+    // Now using a prebuilt test jarjar -- found in the forklift-test-consumer
+    // make sure that we can deploy it that contains multiple classes that have some consumers
+    // that consume queues and some consuming topics
+    @Test
+    public void testDeployJarJar() throws IOException {
+    	File f = new File("src/test/resources/forklift-jarjar-consumer-0.1-binks.jar");
+        Deployment d = new Deployment(f);
+        assertNotNull(d);
+        assertTrue(d.isJar());
+        assertEquals(4, d.getQueues().size());
+        assertEquals(2, d.getTopics().size());
+        // Make sure we don't accidentally delete on deploy
+        assertTrue(f.exists());
+    }
+}
