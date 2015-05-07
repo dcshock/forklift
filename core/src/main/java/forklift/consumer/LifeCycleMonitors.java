433aac303fbed76aef499db8ca07c6ce11cef584
dthompson <dthompson@sofi.org>
2015-05-07 14:06:54 -0600

    Add some logging when monitor can't be added. Throw runtime exception in case where synchronization might be broke and we'll have issues (helps with testing). Log in deregister.

--- core/src/main/java/forklift/consumer/LifeCycleMonitors.java
+++ core/src/main/java/forklift/consumer/LifeCycleMonitors.java
@@ -1,6 +1,5 @@
 package forklift.consumer;
 
-import forklift.consumer.ProcessStep;
 import forklift.decorators.LifeCycle;
 
 import org.slf4j.Logger;
@@ -18,120 +17,132 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LifeCycleMonitors {
-	private static final Logger log = LoggerFactory.getLogger(LifeCycleMonitors.class);
+    private static final Logger log = LoggerFactory.getLogger(LifeCycleMonitors.class);
 
-	private static class Monitor {
-		Class<?> clazz;
-		Method method;
-		Object instance;
-	}
+    private static class Monitor {
+        Class<?> clazz;
+        Method method;
+        Object instance;
+    }
 
-	private static AtomicInteger calls;
-	private static final Map<ProcessStep, List<Monitor>> monitors;
-	static {
-		calls = new AtomicInteger(0);
+    private static AtomicInteger calls;
+    private static final Map<ProcessStep, List<Monitor>> monitors;
+    static {
+        calls = new AtomicInteger(0);
 
-		// Let's make sure we don't have to initialize anything at the root level later.
-		monitors = new HashMap<>();
-		monitors.put(ProcessStep.Pending, new ArrayList<Monitor>());
-		monitors.put(ProcessStep.Validating, new ArrayList<Monitor>());
-		monitors.put(ProcessStep.Invalid, new ArrayList<Monitor>());
-		monitors.put(ProcessStep.Processing, new ArrayList<Monitor>());
-		monitors.put(ProcessStep.Retrying, new ArrayList<Monitor>());
-		monitors.put(ProcessStep.MaxRetriesExceeded, new ArrayList<Monitor>());
-		monitors.put(ProcessStep.Error, new ArrayList<Monitor>());
-		monitors.put(ProcessStep.Complete, new ArrayList<Monitor>());
-	}
+        // Let's make sure we don't have to initialize anything at the root level later.
+        monitors = new HashMap<>();
+        monitors.put(ProcessStep.Pending, new ArrayList<Monitor>());
+        monitors.put(ProcessStep.Validating, new ArrayList<Monitor>());
+        monitors.put(ProcessStep.Invalid, new ArrayList<Monitor>());
+        monitors.put(ProcessStep.Processing, new ArrayList<Monitor>());
+        monitors.put(ProcessStep.Retrying, new ArrayList<Monitor>());
+        monitors.put(ProcessStep.MaxRetriesExceeded, new ArrayList<Monitor>());
+        monitors.put(ProcessStep.Error, new ArrayList<Monitor>());
+        monitors.put(ProcessStep.Complete, new ArrayList<Monitor>());
+    }
 
-	public static void register(Class<?> clazz) {
-		synchronized (monitors) {
-			while (calls.get() > 0) {
-			    try {	
-					monitors.wait(10);
-				} catch (InterruptedException ignored) {}
-			}
+    public static void register(Class<?> clazz) {
+        synchronized (monitors) {
+            while (calls.get() > 0) {
+                try {
+                    monitors.wait(10);
+                } catch (InterruptedException ignored) {}
+            }
+            if (calls.get() != 0)
+                throw new RuntimeException("Registering LifeCycleMonitor during active call.");
 
-			// For methods that aren't static we'll need to have an instance around.
-			Object instance = null;
+            // For methods that aren't static we'll need to have an instance around.
+            Object instance = null;
 
-			// Get all of the methods that may have lifecycle annotations. 
-			for (Method m : clazz.getDeclaredMethods()) {
-				// Check to see if we need an instance around.
-				final boolean staticMethod = Modifier.isStatic(m.getModifiers());
-				if (!staticMethod && instance == null) {
-					try {
-						instance = clazz.newInstance();
-					} catch (IllegalAccessException | InstantiationException ignored) {
-						return;
-					}
-				}
+            // Get all of the methods that may have lifecycle annotations.
+            for (Method m : clazz.getDeclaredMethods()) {
+                // Check to see if we need an instance around.
+                final boolean staticMethod = Modifier.isStatic(m.getModifiers());
+                if (!staticMethod && instance == null) {
+                    try {
+                        instance = clazz.newInstance();
+                    } catch (IllegalAccessException | InstantiationException ignored) {
+                        return;
+                    }
+                }
 
-				for (Annotation a : m.getAnnotationsByType(LifeCycle.class)) {
-					log.info("Adding Monitor.... {}-{}", clazz, a);
-					m.setAccessible(true);
+                for (Annotation a : m.getAnnotationsByType(LifeCycle.class)) {
+                    log.info("Adding Monitor.... {}-{}", clazz, a);
+                    m.setAccessible(true);
 
-					final Monitor monitor = new Monitor();
-					monitor.clazz = clazz;
-					monitor.method = m;
+                    final Monitor monitor = new Monitor();
+                    monitor.clazz = clazz;
+                    monitor.method = m;
 
-					// Only store the instance if this isn't static so that the method can be
-					// called later on.
-					if (!staticMethod)
-						monitor.instance = instance;
+                    // Only store the instance if this isn't static so that the method can be
+                    // called later on.
+                    if (!staticMethod)
+                        monitor.instance = instance;
 
-					final LifeCycle lifeCycle = (LifeCycle)a;
-					monitors.get(lifeCycle.value()).add(monitor);
-				}
-			}
-		}
-	}
+                    final LifeCycle lifeCycle = (LifeCycle)a;
+                    monitors.get(lifeCycle.value()).add(monitor);
+                }
+            }
+        }
+    }
 
-	public static void deregister(final Class<?> clazz) {
-		synchronized (monitors) {
-			while (calls.get() > 0) {
-			    try {	
-					monitors.wait(10);
-				} catch (InterruptedException ignored) {}
-			}
+    public static void deregister(final Class<?> clazz) {
+        synchronized (monitors) {
+            while (calls.get() > 0) {
+                try {
+                    monitors.wait(10);
+                } catch (InterruptedException ignored) {}
+            }
+            if (calls.get() != 0)
+                throw new RuntimeException("Registering LifeCycleMonitor during active call.");
 
-			// Stream through the monitors hash, and remove any underlying list elements
-			// that are represented by the deregistering class.
-			monitors.values().stream().forEach(list -> {
-				// Use an iterator to avoid concurrent mod exceptions
-				final Iterator<Monitor> it = list.iterator();
-		    	while (it.hasNext()) {
-		    		final Monitor monitor = it.next();
-		    		if (monitor.clazz == clazz)
-		    			it.remove();
-		    	}
-			});
-		}
-	}
+            // Stream through the monitors hash, and remove any underlying list elements
+            // that are represented by the deregistering class.
+            monitors.values().stream().forEach(list -> {
+                // Use an iterator to avoid concurrent mod exceptions
+                final Iterator<Monitor> it = list.iterator();
+                while (it.hasNext()) {
+                    final Monitor monitor = it.next();
+                    if (monitor.clazz == clazz) {
+                        log.info("Removing Monitor.... {}-{}", monitor.clazz, monitor.method);
+                        it.remove();
+                    }
+                }
+            });
+        }
+    }
 
-	public static void call(ProcessStep step, MessageRunnable mr) {
-		if (calls.get() == 0) {
-			synchronized (monitors) {
-				calls.incrementAndGet();
-			}	
-		} else {
-			calls.incrementAndGet();
-		}
+    public static void call(ProcessStep step, MessageRunnable mr) {
+        if (calls.get() == 0) {
+            synchronized (monitors) {
+                calls.incrementAndGet();
+            }
+        } else {
+            calls.incrementAndGet();
+        }
 
-		try {
-			// Find calls that match the step we're currently processing.
-			monitors.keySet().stream().filter(key -> key == step)
-			    .map(key -> monitors.get(key))
-			    .forEach(list -> {
-			    	list.forEach(monitor -> {
-			    		try {
-			    			// In the case of static methods the registration() method leaves the 
-			    			// instance as null.
-				    		monitor.method.invoke(monitor.instance, mr);
-				    	} catch (IllegalAccessException | InvocationTargetException ignored) {}
-			    	});
-			    });
-		} finally {
-			calls.decrementAndGet();		
-		}
-	}
+        try {
+            // Find calls that match the step we're currently processing.
+            monitors.keySet().stream().filter(key -> key == step)
+                .map(key -> monitors.get(key))
+                .forEach(list -> {
+                    list.forEach(monitor -> {
+                        try {
+                            // In the case of static methods the registration() method leaves the
+                            // instance as null.
+                            monitor.method.invoke(monitor.instance, mr);
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            log.debug("Error invoking LifeCycle Monitor", e);
+                        }
+                    });
+                });
+        } finally {
+            calls.decrementAndGet();
+        }
+    }
+
+    public static Integer getCalls() {
+        return calls.get();
+    }
 }
