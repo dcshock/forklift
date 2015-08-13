d37deec7ad33360b142285b5f482ba9412354c2a
Peter Wolf <pwolf@sofi.org>
2015-08-13 09:14:36 -0600

    Changed how we delegate libraries

--- core/src/main/java/forklift/classloader/ChildFirstClassLoader.java
+++ core/src/main/java/forklift/classloader/ChildFirstClassLoader.java
@@ -26,9 +26,8 @@ public class ChildFirstClassLoader extends URLClassLoader {
 
         // There are some classes that we can't go to the child first to resolve. In these instances we'll intercept the call
         // and delegate it to the system classloader. All of these relate to base jre libraries that are bundled in java.
-//        List<String> libraries = Arrays.asList("java", "com.sun", "sun", "org.xml", "org.w3c");
-//        if (libraries.stream().anyMatch(s -> name.startsWith(s))) {
-        if (name.startsWith("java") || name.startsWith("com.sun") || name.startsWith("sun") || name.startsWith("org.xml") || name.startsWith("org.w3c")) {
+        List<String> libraries = Arrays.asList("java", "com.sun", "sun", "org.xml", "org.w3c");
+        if (libraries.stream().anyMatch(s -> name.startsWith(s))) {
             try {
                 c = system.loadClass(name);
             } catch (ClassNotFoundException e3) {
