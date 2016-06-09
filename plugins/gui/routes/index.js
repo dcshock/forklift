b3b9c25ec65b658b13c8fd9422ff56c72bfce20a
ShaneIsrael <sisrael@sofi.org>
2016-06-09 10:29:25 -0600

    changed route to get

--- plugins/gui/routes/index.js
+++ plugins/gui/routes/index.js
@@ -60,7 +60,7 @@ router.get('/auth/google/callback',
     })
 );
 
-router.post('/sendDailySummary/', function(req, res) {
+router.get('/sendDailySummary/', function(req, res) {
     var authHeader = req.headers['authorization'];
     var token = null;
     var tokenType = null;
