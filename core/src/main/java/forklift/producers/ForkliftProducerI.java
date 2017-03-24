bc26a5c82e4e981f3adae6dcd1f91f7b319f4921
Matt Conroy <mconroy@sofi.com>
2017-03-23 20:44:52 -0600

    - Update the producer interface to remove jms and mod props to string values.

--- core/src/main/java/forklift/producers/ForkliftProducerI.java
+++ core/src/main/java/forklift/producers/ForkliftProducerI.java
@@ -2,25 +2,30 @@ package forklift.producers;
 
 import forklift.connectors.ForkliftMessage;
 import forklift.message.Header;
-
 import java.io.Closeable;
-import java.util.List;
 import java.util.Map;
-
-import javax.jms.Destination;
-import javax.jms.Message;
-import javax.jms.MessageProducer;
 
 public interface ForkliftProducerI extends Closeable {
     String send(String message) throws ProducerException;
+
     String send(ForkliftMessage message) throws ProducerException;
+
     String send(Object message) throws ProducerException;
+
     String send(Map<String, String> message) throws ProducerException;
-    String send(Map<Header, Object> headers, 
-                Map<String, Object> properties,
+
+    String send(Map<String, String> properties,
                 ForkliftMessage message) throws ProducerException;
+
+    String send(Map<Header, Object> headers, 
+                Map<String, String> properties,
+                ForkliftMessage message) throws ProducerException;
+
     Map<Header, Object> getHeaders() throws ProducerException;
+  
     void setHeaders(Map<Header, Object> headers) throws ProducerException;
-    Map<String, Object> getProperties() throws ProducerException;
-    void setProperties(Map<String , Object> properties) throws ProducerException;
+
+    Map<String, String> getProperties() throws ProducerException;
+
+    void setProperties(Map<String, String> properties) throws ProducerException;
 }  
