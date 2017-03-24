54d1f58f64828d82cfcaf5403c6fec4e01e13288
Matt Conroy <mconroy@sofi.com>
2017-03-23 20:20:26 -0600

    - Remove unused AuditLog.java - Implement non-jms versions of the forklift connector and message.

--- core/src/main/java/forklift/connectors/ForkliftConnectorI.java
+++ core/src/main/java/forklift/connectors/ForkliftConnectorI.java
@@ -3,23 +3,11 @@ package forklift.connectors;
 import forklift.consumer.ForkliftConsumerI;
 import forklift.producers.ForkliftProducerI;
 
-import javax.jms.Connection;
-import javax.jms.Message;
-
 public interface ForkliftConnectorI {
     void start() throws ConnectorException;
     void stop() throws ConnectorException;
-    Connection getConnection() throws ConnectorException;
-//    Session getSession() throws ConnectorException;
     ForkliftConsumerI getQueue(String name) throws ConnectorException;
     ForkliftConsumerI getTopic(String name) throws ConnectorException;
     ForkliftProducerI getQueueProducer(String name);
     ForkliftProducerI getTopicProducer(String name);
-
-    /**
-     * Convert a jms message to a forklift message.
-     * @param m - the message to process
-     * @return - a new ForkliftMessage.
-     */
-    ForkliftMessage jmsToForklift(Message m);
 }
--- core/src/main/java/forklift/connectors/ForkliftMessage.java
+++ core/src/main/java/forklift/connectors/ForkliftMessage.java
@@ -1,33 +1,28 @@
 package forklift.connectors;
 
-import forklift.message.Header;
-
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.jms.Message;
-
 public class ForkliftMessage {
-    private Message jmsMsg;
-    private String msg;
-    private boolean flagged;
-    private String warning;
-    private Map<Header, Object> headers;
-    private Map<String, Object> properties;
+    private String id;
+    protected String msg;
+    protected boolean flagged;
+    protected String warning;
+    protected Map<String, String> properties = new HashMap<>();
 
     public ForkliftMessage() {
-    }
-
-    public ForkliftMessage(Message jmsMsg) {
-        this.jmsMsg = jmsMsg;
     }
 
     public ForkliftMessage(String msg) {
         this.setMsg(msg);
     }
 
-    public Message getJmsMsg() {
-        return jmsMsg;
+    public boolean acknowledge() throws ConnectorException {
+        return true;
+    }
+
+    public String getId() {
+        return null;
     }
 
     public String getMsg() {
@@ -54,25 +49,14 @@ public class ForkliftMessage {
         this.flagged = flagged;
     }
 
-    public void setHeaders(Map<Header, Object> headers) {
+    public void setProperties(Map<String, String> properties) {
         // Get rid of unmodifiable.
-        final Map<Header, Object> newHeaders = new HashMap<>();
-        headers.keySet().stream().forEach(key -> newHeaders.put(key, headers.get(key)));
-        this.headers = newHeaders;
-    }
-
-    public Map<Header, Object> getHeaders() {
-        return headers;
-    }
-
-    public void setProperties(Map<String, Object> properties) {
-        // Get rid of unmodifiable.
-        final Map<String, Object> newProps = new HashMap<>();
+        final Map<String, String> newProps = new HashMap<>();
         properties.keySet().stream().forEach(key -> newProps.put(key, properties.get(key)));
         this.properties = newProps;
     }
 
-    public Map<String, Object> getProperties() {
-        return properties;
+    public Map<String, String> getProperties() {
+        return this.properties;
     }
 }
