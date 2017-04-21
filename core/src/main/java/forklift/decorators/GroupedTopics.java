8e737b9c971078d09c69d2c11b90dfb6880b042b
Bridger Howell <BlazeCyndaquil@gmail.com>
2017-04-21 14:21:29 -0600

    Add GroupedTopic annotation and source

---
+++ core/src/main/java/forklift/decorators/GroupedTopics.java
@@ -0,0 +1,15 @@
+package forklift.decorators;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@SourceTypeContainer
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface GroupedTopics {
+    GroupedTopic[] value();
+}
---
+++ core/src/main/java/forklift/source/GroupedTopicSource.java
@@ -0,0 +1,66 @@
+package forklift.source;
+
+import forklift.decorators.GroupedTopic;
+
+import java.util.Objects;
+
+/**
+ * A more easily usable form of the annotation {@code @GroupTopic}.
+ */
+public class GroupedTopicSource implements SourceI {
+    private final String name;
+    private String group;
+
+    public GroupedTopicSource(String name, String group) {
+        this.name = name;
+        this.group = group;
+    }
+
+    public GroupedTopicSource(GroupedTopic topic) {
+        this.name = topic.name();
+        this.group = topic.group();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    /**
+     * Determines whether or not the current group name is a null-like value, and should be changed.
+     *
+     * @return whether a group name is specified
+     */
+    public boolean groupSpecified() {
+        return group == null || group.isEmpty();
+    }
+
+    /**
+     * Allow changing the group name here, so the group name can be ultimiately
+     * determined by the connector.
+     *
+     * @param group the new group to use
+     */
+    public void overrideGroup(String group) {
+        this.group = group;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof GroupedTopicSource))
+            return false;
+        GroupedTopicSource that = (GroupedTopicSource) o;
+
+        return Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public String toString() {
+        return "GroupedTopic@Source(" + name + ")";
+    }
+}
