170e4ed1b6c8da6da6284abb4417985ca2599f49
Bridger Howell <BlazeCyndaquil@gmail.com>
2017-05-17 13:49:22 -0600

    Upgrade javadoc

--- core/src/main/java/forklift/connectors/ForkliftSerializer.java
+++ core/src/main/java/forklift/connectors/ForkliftSerializer.java
@@ -2,6 +2,10 @@ package forklift.connectors;
 
 import forklift.source.SourceI;
 
+/**
+ * An interface that encapsulates the logic for serializing and deserializing objects,
+ * potentially making a distinction between serializing to different sources.
+ */
 public interface ForkliftSerializer {
     /**
      * Serialize the given object to bytes.
@@ -17,7 +21,7 @@ public interface ForkliftSerializer {
      *
      * @param <T> the type expected from the deserialized object
      * @param source the source for which the bytes were destined
-     * @param bytes the bytes to deserizlie
+     * @param bytes the bytes to deserialize
      * @return the bytes deserialized into an object
      */
     public <T> T deserializeForSource(SourceI source, byte[] bytes);
--- core/src/main/java/forklift/consumer/wrapper/RoleInputConsumerWrapper.java
+++ core/src/main/java/forklift/consumer/wrapper/RoleInputConsumerWrapper.java
@@ -4,12 +4,37 @@ import forklift.consumer.ForkliftConsumerI;
 import forklift.connectors.ConnectorException;
 import forklift.connectors.ForkliftMessage;
 
+/**
+ * A consumer that maps the input from a given consumer according to the
+ * serialized format for {@link RoleInputMessage role messages}.
+ * <br>
+ * This mapping function for role input is a crucial piece that allows
+ * {@link forklift.source.sources.RoleInputSource RoleInputSource}s to be
+ * backed by any arbitrary source.
+ */
 public class RoleInputConsumerWrapper implements ForkliftConsumerI {
     private ForkliftConsumerI consumer;
+
+    /**
+     * Creates a consumer that delegates message retrieval to the
+     * given consumer and maps whatever the consumer outputs based on the
+     * assumption that all of the messages returned from the given consumer
+     * were serialized as {@link RoleInputMessage}s.
+     *
+     * @param consumer the consumer to get unmapped messages from
+     */
     public RoleInputConsumerWrapper(ForkliftConsumerI consumer) {
         this.consumer = consumer;
     }
 
+    /**
+     * Delegates to the underlying consumer, and tries to extract a role message
+     * from any received messages.
+     *
+     * @param timeout the timeout of the underlying consumer
+     * @return the
+     * @throws ConnectorException if a connector error occurs in the underlying consumer
+     */
     @Override
     public ForkliftMessage receive(long timeout) throws ConnectorException {
         final ForkliftMessage initialMessage = consumer.receive(timeout);
@@ -20,6 +45,11 @@ public class RoleInputConsumerWrapper implements ForkliftConsumerI {
         return roleInputMessage.toForkliftMessage(initialMessage);
     }
 
+    /**
+     * Closees the underlying consumer.
+     *
+     * @throws ConnectorException if a connector error occurs in the underlying consumer
+     */
     @Override
     public void close() throws ConnectorException {
         consumer.close();
