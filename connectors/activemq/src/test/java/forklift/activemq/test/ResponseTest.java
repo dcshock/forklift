fe86c20711fd904c928a5c480a4cb9c206efd666
Bridger Howell <BlazeCyndaquil@gmail.com>
2017-05-24 10:58:48 -0600

    Add timeouts to response tests

--- connectors/activemq/src/test/java/forklift/activemq/test/ResponseTest.java
+++ connectors/activemq/src/test/java/forklift/activemq/test/ResponseTest.java
@@ -27,6 +27,9 @@ public class ResponseTest {
         TestServiceManager.stop();
     }
 
+    private static final int maxTimeouts = 5;
+    private int timeouts = 0;
+
 //    @Test
     public void testStringResp() throws Exception {
         final ForkliftConnectorI connector = TestServiceManager.getConnector();
@@ -84,7 +87,11 @@ public class ResponseTest {
         ResponseConsumerMap.resolver = resolver;
         final Consumer c = new Consumer(ResponseConsumerMap.class, TestServiceManager.getForklift());
         c.setOutOfMessages((listener) -> {
-            listener.shutdown();
+            timeouts++;
+
+            if (futures.stream().allMatch(future -> future.isDone()) || timeouts > maxTimeouts) {
+                listener.shutdown();
+            }
         });
         c.listen();
 
@@ -122,7 +129,11 @@ public class ResponseTest {
         ResponseConsumerObj.resolver = resolver;
         final Consumer c = new Consumer(ResponseConsumerObj.class, TestServiceManager.getForklift());
         c.setOutOfMessages((listener) -> {
-            listener.shutdown();
+            timeouts++;
+
+            if (futures.stream().allMatch(future -> future.isDone()) || timeouts > maxTimeouts) {
+                listener.shutdown();
+            }
         });
         c.listen();
 
