77379c9d3bf75b96a8558a8dadcea0deadf547af
Bridger Howell <BlazeCyndaquil@gmail.com>
2017-06-07 14:45:08 -0600

    Make GroupedTopic hashable

--- core/src/main/java/forklift/source/sources/GroupedTopicSource.java
+++ core/src/main/java/forklift/source/sources/GroupedTopicSource.java
@@ -51,6 +51,11 @@ public class GroupedTopicSource extends ActionSource {
     }
 
     @Override
+    public int hashCode() {
+        return Objects.hash(name, group);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
