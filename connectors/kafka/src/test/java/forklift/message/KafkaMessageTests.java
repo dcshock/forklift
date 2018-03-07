1c2ca31a9e9228c6ad20a388c35d3aa302fae5d1
Bridger Howell <BlazeCyndaquil@gmail.com>
2018-03-06 18:16:31 -0700

    Add wrong-generation message detection

--- connectors/kafka/src/test/java/forklift/message/KafkaMessageTests.java
+++ connectors/kafka/src/test/java/forklift/message/KafkaMessageTests.java
@@ -3,9 +3,13 @@ package forklift.message;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 import forklift.connectors.ConnectorException;
 import forklift.controller.KafkaController;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,27 +19,58 @@ public class KafkaMessageTests {
     private KafkaController controller;
     private KafkaMessage message;
     private ConsumerRecord record;
+    private Generation generation;
 
     @Before
     public void setup() {
         controller = mock(KafkaController.class);
         record = new ConsumerRecord("testTopic", 0, 1L, "key", "value");
-        message = new KafkaMessage(controller, record);
+        generation = new Generation();
+        generation.assignNextGeneration();
+
+        message = new KafkaMessage(controller, record, generation);
     }
 
     @Test
     public void acknowledgeFalseTest() throws ConnectorException, InterruptedException {
-
         when(controller.acknowledge(record)).thenReturn(false);
-        boolean acknowledged = message.acknowledge();
-        assertFalse(acknowledged);
 
+        boolean acknowledged = message.acknowledge();
+
+        assertFalse(acknowledged);
+        verify(controller).acknowledge(record);
     }
 
     @Test
     public void acknowledgeCallsControllerSuccess() throws ConnectorException, InterruptedException {
         when(controller.acknowledge(record)).thenReturn(true);
+
         boolean acknowledged = message.acknowledge();
+
         assertTrue(acknowledged);
+        verify(controller).acknowledge(record);
+    }
+
+    @Test
+    public void acknowledgeFailsWithUnassignedGeneration() throws ConnectorException, InterruptedException {
+        generation.unassign();
+        when(controller.acknowledge(record)).thenReturn(true);
+
+        boolean acknowledged = message.acknowledge();
+
+        assertFalse(acknowledged);
+        verify(controller, never()).acknowledge(record);
+    }
+
+    @Test
+    public void acknowledgeFailsWithOldGeneration() throws ConnectorException, InterruptedException {
+        generation.unassign();
+        generation.assignNextGeneration();
+        when(controller.acknowledge(record)).thenReturn(true);
+
+        boolean acknowledged = message.acknowledge();
+
+        assertFalse(acknowledged);
+        verify(controller, never()).acknowledge(record);
     }
 }
