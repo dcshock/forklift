c677f8431866ff21e9481dcfd18dded54aab75dd
Bridger Howell <BlazeCyndaquil@gmail.com>
2018-03-08 19:20:53 -0700

    Handle blocking rejected tasks more safely
    Notably, if the work queue is cleared before shutting down, it's
    possible that a late item could get to execute out-of-order.
    
    As for the handler itself, if the thread pool is shutdown on a thread
    separate from the rejected execution handler, it could be queued after
    a shutdown, unless the handler checks whether the pool is shutdown
    between the work queue having space and queuing the task.

--- core/src/main/java/forklift/concurrent/BlockingRejectedExecutionHandler.java
+++ core/src/main/java/forklift/concurrent/BlockingRejectedExecutionHandler.java
@@ -8,13 +8,20 @@ import java.util.concurrent.RejectedExecutionHandler;
  * tasks from a running thread pool by blocking.
  */
 public class BlockingRejectedExecutionHandler implements RejectedExecutionHandler {
+    private static final Runnable NO_OP = () -> {};
+
     @Override
     public void rejectedExecution(Runnable runnable, ThreadPoolExecutor executor) {
-        while (!executor.isShutdown()) {
-            try {
-                executor.getQueue().put(runnable);
-                return;
-            } catch (InterruptedException ignored) {}
-        }
+        if (executor.isShutdown()) { return; }
+
+        try {
+            // wait for space in the queue
+            executor.getQueue().put(NO_OP);
+            executor.remove(NO_OP);
+
+            if (!executor.isShutdown()) {
+                executor.execute(runnable);
+            }
+        } catch (InterruptedException ignored) {}
     }
 }
